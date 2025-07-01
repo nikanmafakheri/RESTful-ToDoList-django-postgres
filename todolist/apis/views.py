8436dad9212515fa7491334924251664fa400ba7
nikan <nikanmafakherii@gmail.com>
2025-07-01 16:39:40 +0330

    docs: add notes

--- todolist/apis/views.py
+++ todolist/apis/views.py
@@ -34,15 +34,14 @@ def CreatePost(request):
 #Read
 @api_view(['GET'])
 def ViewPost(request):
-  
+  #checking for the params in URL
   if request.query_params:
     post = Post.objects.filter(**request.query_params.dict())
   else:
     post = Post.objects.all()  
-    
+  # Check for posts to not be empty  
   if post :
     serializer = PostSerializer(post , many=True)
     return Response(serializer.data)
   else :
     return Response(status=status.HTTP_404_NOT_FOUND)
-  
