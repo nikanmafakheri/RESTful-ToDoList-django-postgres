b76d452adde02ff049970f06523e4da84714ce4f
nikan <nikanmafakherii@gmail.com>
2025-07-06 14:17:57 +0330

    feat: add needed packages and modules for the view

--- todolist/apis/views.py
+++ todolist/apis/views.py
@@ -1,10 +1,11 @@
 # CRUD Operations
 from django.shortcuts import get_object_or_404
 from .models import Task
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view , permission_classes
 from rest_framework.response import Response
 from .serializers import TaskSerializer
 from rest_framework import serializers , status
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
 def ApiOverView(request):
